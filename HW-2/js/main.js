951166d02b9e6343668e69b667b222a7cc10458a
mackentoshen <strokanvalentyn@gmail.com>
2018-10-26 17:54:37 +0300

    done without ""

---
+++ HW-2/js/main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var a = 'function test(){ alert(); }';
+var b = 'function test)({ alert(); }';
+
+function js(text){
+    var i,
+        l = text.length,
+        char,
+        last,
+        stack = [];
+
+    for(i=0; i<l; i++){
+        char = text[i];
+
+        if(char == "{" || char == "("){
+            stack.push(char);
+        }else if(char == "}" || char == ")"){
+            if(stack.length>0){
+                last = stack[stack.length-1];
+                if ((char == '}' && last == "{") || (char == ')' && last == '(')) {
+                    stack.pop();
+                }
+            }
+        }
+    }
+
+    return stack.length == 0;
+}
+
+console.log(js(a));
+console.log(js(b));
+
+// alert(js("function test(){ alert(); }"));
+// alert(js("function test)({ alert(); }"));
